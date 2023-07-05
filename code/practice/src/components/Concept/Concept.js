2cb284aa97b588422381c290577bb168b1ff2a10
Szymon Markiewicz <szymon.markiewicz@xperi.com>
2023-07-05 16:59:11 +0200

    Finish first practice section

---
+++ code/practice/src/components/Concept/Concept.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function Concept({concept}) {
+    return (
+        <li className="concept">
+            <img src={concept.image} alt={concept.title} />
+            <h2>{concept.title}</h2>
+            <p>{concept.description}</p>
+        </li>
+    );
+}
