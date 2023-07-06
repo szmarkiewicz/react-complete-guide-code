9325859af48f650a29ffbc972a8b6eca18a3399d
Szymon Markiewicz <szymon.markiewicz@xperi.com>
2023-07-06 13:35:38 +0200

    Complete Assignment 2

---
+++ code/assignment/src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from "../Expenses/ExpensesFilter";
+
+const Expenses = (props) => {
+    const [year, setYear] = React.useState('2020');
+
+    const onYearChange = (year) => {
+        setYear(year);
+        console.log(year);
+    }
+
+    return (
+        <div>
+            <ExpensesFilter selected={year} onYearChange={onYearChange}/>
+            <Card className="expenses">
+                <ExpenseItem
+                    title={props.items[0].title}
+                    amount={props.items[0].amount}
+                    date={props.items[0].date}
+                />
+                <ExpenseItem
+                    title={props.items[1].title}
+                    amount={props.items[1].amount}
+                    date={props.items[1].date}
+                />
+                <ExpenseItem
+                    title={props.items[2].title}
+                    amount={props.items[2].amount}
+                    date={props.items[2].date}
+                />
+                <ExpenseItem
+                    title={props.items[3].title}
+                    amount={props.items[3].amount}
+                    date={props.items[3].date}
+                />
+            </Card>
+        </div>
+    );
+}
+
+export default Expenses;
