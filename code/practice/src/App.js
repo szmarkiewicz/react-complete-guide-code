3a8d77fc622eb74402b4b8c70318cf607114ee37
Szymon Markiewicz <szymon.markiewicz@xperi.com>
2023-07-06 12:32:07 +0200

    Redundancy deleted

--- code/practice/src/App.js
+++ code/practice/src/App.js
@@ -1,4 +1,3 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
 import componentsImage from './assets/images/components.png';
 import stateImage from './assets/images/state.png';
 import eventsImage from './assets/images/events.png';
